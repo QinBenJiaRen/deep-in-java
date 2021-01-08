c77568265b410b08ba3587673da7552cdda4a3ba
贾海锋 <jia_haifeng@yeah.net>
2021-01-08 17:38:46 +0800

    LeetCode_239 窗口滑动

--- deep-in-java/deep-in-dataStruct/src/main/java/Problems/Hard/LeetCode_239.java
+++ deep-in-java/deep-in-dataStruct/src/main/java/Problems/Hard/LeetCode_239.java
@@ -1,5 +1,8 @@
 package Problems.Hard;
 
+import java.util.Comparator;
+import java.util.PriorityQueue;
+
 /**
  * @Description TODO
  * @Author jia_h
@@ -20,7 +23,7 @@ public class LeetCode_239 {
      * 暴力求解
      * 可以解决，但是会超时
      * */
-    private static int[] maxSlidingWindow(int[] nums, int k) {
+    private static int[] maxSlidingWindow_1(int[] nums, int k) {
 
         int[] res = new int[nums.length - k + 1];
         for (int  i = 0; i < nums.length - k + 1; i++) {
@@ -34,5 +37,30 @@ public class LeetCode_239 {
             res[i] = max;
         }
         return res;
+    }
+
+    /**
+     * 优先队列
+     * */
+    public int[] maxSlidingWindow_2(int[] nums, int k) {
+        int n = nums.length;
+        PriorityQueue<int[]> pq = new PriorityQueue<int[]>(new Comparator<int[]>() {
+            public int compare(int[] pair1, int[] pair2) {
+                return pair1[0] != pair2[0] ? pair2[0] - pair1[0] : pair2[1] - pair1[1];
+            }
+        });
+        for (int i = 0; i < k; ++i) {
+            pq.offer(new int[]{nums[i], i});
+        }
+        int[] ans = new int[n - k + 1];
+        ans[0] = pq.peek()[0];
+        for (int i = k; i < n; ++i) {
+            pq.offer(new int[]{nums[i], i});
+            while (pq.peek()[1] <= i - k) {
+                pq.poll();
+            }
+            ans[i - k + 1] = pq.peek()[0];
+        }
+        return ans;
     }
 }
