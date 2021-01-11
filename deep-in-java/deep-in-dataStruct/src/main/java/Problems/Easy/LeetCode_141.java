45c654e181cba1d747776cd86033bc8d56c9ae49
贾海锋 <jia_haifeng@yeah.net>
2021-01-11 19:18:03 +0800

    LeetCode_141

---
+++ deep-in-java/deep-in-dataStruct/src/main/java/Problems/Easy/LeetCode_141.java
@@ -0,0 +1,45 @@
+package Problems.Easy;
+
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
+/**
+ * @Description
+ * @Author jia_h
+ * @Date 2021/1/11 9:50
+ * @Version 1.0
+ */
+public class LeetCode_141 {
+
+    /**
+     * url:https://leetcode-cn.com/problems/linked-list-cycle/
+     * */
+
+    public static void main(String[] args) {
+
+    }
+
+    class ListNode{
+        int val;
+        ListNode next;
+        ListNode(int x) {
+            val = x;
+            next = null;
+        }
+    }
+
+    public boolean hasCycle(ListNode head) {
+        Map<ListNode, Integer> map = new HashMap<>();
+        while (head != null) {
+            if (map.get(head) == null) {
+                map.put(head, 1);
+                head = head.next;
+            } else {
+                return true;
+            }
+        }
+        return false;
+    }
+}
--- deep-in-java/deep-in-dataStruct/src/main/java/Problems/Hard/LeetCode_239.java
+++ deep-in-java/deep-in-dataStruct/src/main/java/Problems/Hard/LeetCode_239.java
@@ -46,7 +46,7 @@ public class LeetCode_239 {
         PriorityQueue<int[]> pq = new PriorityQueue<>(new Comparator<int[]>() {
             @Override
             public int compare(int[] o1, int[] o2) {
-                return o1[1] != o2[1] ? o2[1] - o1[1] : o2[1] - o1[1];
+                return o1[0] != o2[0] ? o2[0] - o1[0] : o2[1] - o1[1];
             }
         });
         int[] ans = new int[nums.length - k + 1];
