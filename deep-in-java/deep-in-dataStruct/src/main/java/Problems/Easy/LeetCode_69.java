1cc3752ece3e24fd53e45bcfedb89755d49d44de
贾海锋 <jia_haifeng@yeah.net>
2021-01-13 09:49:27 +0800

    LeetCode_69

--- deep-in-java/deep-in-dataStruct/src/main/java/Problems/Easy/LeetCode_69.java
+++ deep-in-java/deep-in-dataStruct/src/main/java/Problems/Easy/LeetCode_69.java
@@ -5,27 +5,26 @@ import java.util.Map;
 /**
  * 69. Sqrt(x)
  * Implement int sqrt(int x).
- *
+ * <p>
  * Compute and return the square root of x, where x is guaranteed to be a non-negative integer.
- *
+ * <p>
  * Since the return type is an integer, the decimal digits are truncated and only the integer part of the result is returned.
- *
+ * <p>
  * Example 1:
- *
+ * <p>
  * Input: 4
  * Output: 2
  * Example 2:
- *
+ * <p>
  * Input: 8
  * Output: 2
  * Explanation: The square root of 8 is 2.82842..., and since
- *              the decimal part is truncated, 2 is returned.
- * */
+ * the decimal part is truncated, 2 is returned.
+ */
 public class LeetCode_69 {
 
     public static void main(String[] args) {
         System.out.println(mySqrt(8));
-        System.out.println(mySqrt2(8));
 
         //System.out.println(Math.sqrt(8));
     }
@@ -34,49 +33,17 @@ public class LeetCode_69 {
         if (x == 0) {
             return 0;
         }
-
-        double mid = 0.0;
-        double left = 0;
-        double right = x;
-        while (left <= right) {
-            mid = (left + right) / 2;
-            if (x / mid > mid) {
-                left = mid;
-                //System.out.printf("x={%s}, left={%s}, right={%s}, mid={%s}\n", x, left, right, mid);
-            }
-            if (x / mid < mid) {
-                right = mid;
-                //System.out.printf("x={%s}, left={%s}, right={%s}, mid={%s}\n", x, left, right, mid);
-            }
-
-            if (Math.abs((x / mid) - mid) <= 0.001) {
-                //System.out.printf("x={%s}, left={%s}, right={%s}, mid={%s}\n", x, left, right, mid);
-                //return (int) Math.floor(mid); //向下取整
-                //System.out.printf("向上取整{%s}, 向下取整{%s}", Math.ceil(mid), Math.floor(mid));
-                //return (int) Math.ceil(mid); //向上取整
-                System.out.println(mid);
-                return (int) mid;
-            }
-        }
-        System.out.printf("向上取整{%s}, 向下取整{%s}", Math.ceil(mid), Math.floor(mid));
-        return (int) mid;
-    }
-
-    public static int mySqrt2(int x) {
-        if(x==0){
-            return 0;
-        }
         int start = 1;
         int end = x;
-        int ans =0;
-        while(start<=end){
-            int mid = (end+start)/2;
-            if(mid <= x/mid){
+        int ans = 0;
+        while (start <= end) {
+            // 防止int类型数据超出范围
+            int mid = start + (end - start) / 2;
+            if (mid <= x / mid) {
                 ans = mid;
-                start = mid+1;
-            }
-            else{
-                end = mid-1;
+                start = mid + 1;
+            } else {
+                end = mid - 1;
             }
         }
         return ans;
