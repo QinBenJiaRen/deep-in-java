02b4d260fe45f95d02712fb20e818a654cc36c17
贾海锋 <jia_haifeng@yeah.net>
2021-01-14 18:26:55 +0800

    LeetCode_1018

--- deep-in-java/deep-in-dataStruct/src/main/java/Problems/Easy/LeetCode_1018.java
+++ deep-in-java/deep-in-dataStruct/src/main/java/Problems/Easy/LeetCode_1018.java
@@ -13,13 +13,17 @@ public class LeetCode_1018 {
 
     public static void main(String[] args) {
 
+        int[] a = new int[] {1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1};
+        System.out.println(prefixDivBy5(a));
     }
 
     public static List<Boolean> prefixDivBy5(int[] A) {
         List<Boolean> ans = new ArrayList<>(A.length);
         int temp = 0;
         for (int value: A) {
-            temp = (temp << 1 + value) % 5;
+            // 判断余数
+            temp = ((temp << 1) + value) % 5;
+            System.out.println(temp);
             ans.add(temp == 0);
         }
         return ans;
