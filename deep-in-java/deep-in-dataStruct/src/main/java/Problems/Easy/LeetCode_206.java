33fbe2896d6a28b54f77a17d4255bd71dc5b5339
贾海锋 <jia_haifeng@yeah.net>
2021-02-03 10:41:33 +0800

    LeetCode_231 单链表反转

---
+++ deep-in-java/deep-in-dataStruct/src/main/java/Problems/Easy/LeetCode_206.java
@@ -0,0 +1,56 @@
+package Problems.Easy;
+
+import java.util.Stack;
+
+/**
+ * @Description 反转链表
+ * @Author jia_h
+ * @Date 2021/2/3 10:01
+ * @Version 1.0
+ */
+public class LeetCode_206 {
+
+    /**
+     * url:https://leetcode-cn.com/problems/reverse-linked-list/
+     * */
+    public static void main(String[] args) {
+
+    }
+
+    /**
+     * 双指针法
+     * prev指针指向空，cur指针当前节点，不断遍历cur
+     * 每遍历一次，cur指向下一个节点
+     * */
+    public ListNode reverseList(ListNode head) {
+        if (head == null) {
+            return head;
+        }
+        ListNode prev = null; // 指向null
+        ListNode cur = head;
+        while (cur != null) {
+            ListNode next = cur.next;
+            //反转关键代码
+            cur.next = prev;
+            prev = cur;
+            cur = next;
+        }
+        return prev;
+    }
+
+    public class ListNode {
+        int val;
+        ListNode next;
+        ListNode() {}
+        ListNode(int val) {
+            this.val = val;
+        }
+        ListNode(int val, ListNode next) {
+            this.val = val;
+            this.next = next;
+        }
+    }
+
+}
+
+
