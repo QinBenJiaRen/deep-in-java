fcee7b8dd6d7a4885f5db6cc76c83d099957cae6
贾海锋 <jia_haifeng@yeah.net>
2021-03-03 09:32:16 +0800

    list转map

--- deep-in-java/deep-in-lambda/src/main/java/ListToMap.java
+++ deep-in-java/deep-in-lambda/src/main/java/ListToMap.java
@@ -1,3 +1,5 @@
+import java.util.HashMap;
+import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.function.Function;
@@ -25,14 +27,33 @@ public class ListToMap {
 
     /**
      * 重复Key
+     * 可能会报java.lang.IllegalStateException: Duplicate key异常
      * */
     public static Map<String, Account> getNameAccountMap(List<Account> list) {
         return list.stream().collect(Collectors.toMap(Account::getName, Function.identity()));
     }
 
     /**
+     * name有可能重复，toMap的重载方法，传入一个合并函数来解决key冲突的问题
+     * */
+    public static Map<String, Account> getNameAccountMap2(List<Account> list) {
+        return list.stream().collect(Collectors.toMap(Account::getName, Function.identity(), (key1, key2) -> key2));
+    }
+
+    /**
      * 收集实体本身Map
      * */
+    public Map<Integer, Account> getIdAccountMap(List<Account> list) {
+        return list.stream().collect(Collectors.toMap(Account::getId, account -> account));
+    }
+
+    /**
+     * 指定具体收集结果的Map类型
+     * */
+    public Map<String, Account> getNameAccountMap3(List<Account> list) {
+        return list.stream().collect(Collectors.toMap(Account::getName, Function.identity(), (k1, k2) -> k2, LinkedHashMap::new));
+    }
+
 
     public class Account {
         private Integer id;
