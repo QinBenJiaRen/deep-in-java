94c8580e7605752b04131f2958c9f4cef8192e08
贾海锋 <jia_haifeng@yeah.net>
2021-03-22 10:25:34 +0800

    平衡二叉树

---
+++ deep-in-java/deep-in-dataStruct/src/main/java/Problems/Easy/树/Offer55II.java
@@ -0,0 +1,27 @@
+package Problems.Easy.树;
+
+/**
+ * @Description 平衡二叉树
+ * @Author jia_h
+ * @Date 2021/3/22 10:23
+ * @Version 1.0
+ */
+public class Offer55II {
+
+    /**
+     * https://leetcode-cn.com/problems/ping-heng-er-cha-shu-lcof/
+     */
+    public static void main(String[] args) {
+
+    }
+
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+    }
+}
