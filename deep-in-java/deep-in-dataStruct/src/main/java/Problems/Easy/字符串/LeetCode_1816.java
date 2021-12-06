1a6bbc8f8ab74919536d9996325add8f0ad2460a
贾海锋 <jia_haifeng@yeah.net>
2021-12-06 15:34:04 +0800

    截断句子

--- deep-in-java/deep-in-dataStruct/src/main/java/Problems/Easy/字符串/LeetCode_1816.java
+++ deep-in-java/deep-in-dataStruct/src/main/java/Problems/Easy/字符串/LeetCode_1816.java
@@ -30,4 +30,28 @@ public class LeetCode_1816 {
             return builder.toString();
         }
     }
+
+    /**
+     * 每个单词的最后都跟随一个空格，因此可以通过统计空格与句子结尾的数目来统计单词个数count。当count==k时，将当前的下标记录到end。
+     * 时间复杂度: O(N)
+     * 空间复杂度: O(1)
+     * */
+    public static String truncateSentence1(String s, int k) {
+        if (null == s) {
+            return s;
+        }
+        int n = s.length();
+        int end = 0;
+        int count = 0;
+        for (int i = 1; i <= n; i++) {
+            if (end == n || s.charAt(i) == ' ') {
+                count++;
+                if (count == k) {
+                    end = i;
+                    break;
+                }
+            }
+        }
+        return s.substring(0, end);
+    }
 }
