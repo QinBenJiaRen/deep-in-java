3346836ec15ddbc0b2c5d9e684962fc40b81e3d3
贾海锋 <jia_haifeng@yeah.net>
2022-12-08 15:06:06 +0800

    Update LC100.java

--- deep-in-java/deep-in-dataStruct/src/main/java/Tree/LC100.java
+++ deep-in-java/deep-in-dataStruct/src/main/java/Tree/LC100.java
@@ -18,9 +18,9 @@ public class Solution {
     public boolean isSameTree(TreeNode p, TreeNode q) {
       if (p == null && q == null) {
         return true;
-      } else if (p == null && q != null) {
+      } else if (p == null || q == null) {
         return false;
-      } else if (p != null && q == null) {
+      } else if (p.val != q.val) {
         return false;
       } else {
         return isSameTree(p.left, q.rigth) && isSameTree(p.right, q.left);
